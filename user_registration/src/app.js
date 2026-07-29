71d3c9766769ea83ac3513dc5d9f0ec5a28a83f2
Sangeet Kumar <ps555239@gmail.com>
2026-07-29 13:45:04 +0530

    update port

--- user_registration/src/app.js
+++ user_registration/src/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const authRoutes = require("./routes/auth");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
